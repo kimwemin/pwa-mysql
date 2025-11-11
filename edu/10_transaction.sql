bf4eb36449bee1f68a28511c6f6a19658166cf9b
kimwemin <kimwemin488@gmail.com>
2025-11-11 10:48:05 +0900

    251111 ddl com

---
+++ edu/10_transaction.sql
@@ -0,0 +1,19 @@
+-- TRANSACTION
+
+-- 트랜잭션 시작
+START TRANSACTION;
+
+-- insert
+INSERT INTO employees (`name`, birth, gender, hire_at)
+VALUES ('미어캣', '2000-01-01', 'M', DATE(NOW()));
+
+-- UPDATE employees SET birth = NOW() WHERE emp_id = 100008;
+
+-- select
+SELECT * FROM employees WHERE `name` = '미어캣';
+
+-- rollback
+ROLLBACK;
+
+-- commit
+COMMIT;
